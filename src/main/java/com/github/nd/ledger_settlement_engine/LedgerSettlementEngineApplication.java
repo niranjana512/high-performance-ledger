13c85854f553b766b20acc19b464ee4d1244d07f
Niranjana <sriniranjana1503@gmail.com>
2026-01-07 16:33:44 +0530

    initial commit

---
+++ src/main/java/com/github/nd/ledger_settlement_engine/LedgerSettlementEngineApplication.java
@@ -0,0 +1,13 @@
+package com.github.nd.ledger_settlement_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerSettlementEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerSettlementEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nd/ledger_settlement_engine/LedgerSettlementEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nd.ledger_settlement_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerSettlementEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
